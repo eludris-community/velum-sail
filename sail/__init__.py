b0463814f67cadc9ce0104a594e973ae410da6e7
Chromosomologist <milan.smts@gmail.com>
2022-11-29 12:10:46 +0100

    feat: add `__init__.py` files for ease of use

---
+++ sail/__init__.py
@@ -0,0 +1,10 @@
+from sail import impl as impl
+from sail import internal as internal
+from sail import traits as traits
+from sail.errors import *
+from sail.impl.command import *
+from sail.impl.command_manager import *
+from sail.internal.typing_utils import Greedy as Greedy
+from sail.internal.undefined import UNDEFINED as UNDEFINED
+from sail.internal.undefined import UndefinedOr as UndefinedOr
+from sail.internal.undefined import UndefinedType as UndefinedType
---
+++ sail/internal/__init__.py
@@ -0,0 +1,4 @@
+from sail.internal.empty import *
+from sail.internal.parser import *
+from sail.internal.typing_utils import *
+from sail.internal.undefined import *
